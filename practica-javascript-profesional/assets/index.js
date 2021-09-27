fa533648b23162b096120a6e34187687b4c3d349
Daniel Bravo <daniel.brv@hotmail.cl>
2021-09-27 16:28:23 -0300

    Se agrega archivo MediaPlayer.js y se exporta como modulo, todo funciona bien

--- practica-javascript-profesional/assets/index.js
+++ practica-javascript-profesional/assets/index.js
@@ -1,25 +1,7 @@
-const video = document.querySelector('video');
-const button = document.querySelector('button');
-const player = new MediaPlayer({ el: video });
+import MediaPlayer from './MediaPlayer.js';
 
-function MediaPlayer(config) {
-    this.media = config.el;
-  }
-  
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
-  
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
-  
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
-  
-  button.onclick = () => player.togglePlay();
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video });
+const button = document.querySelector('button');
+
+button.onclick = () => player.togglePlay();
